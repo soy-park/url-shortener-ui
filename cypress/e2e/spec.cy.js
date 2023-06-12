7f157dbd74cd886610397bce023e0d318ad72aef
Sara Park <sspark106@gmail.com>
2023-06-12 10:47:53 -0600

    feat: Add beforeEach() to stub network request

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,21 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+When a user visits the page, they can view the page title and the existing shortened URLs
+When a user visits the page, they can view the Form with the proper inputs
+When a user fills out the form, the information is reflected in the input fields
+
+describe('Main page', () => {
+  beforeEach(() => {
+    cy.intercept("GET", "http://localhost:3001/api/v1/urls", {
+      statusCode: 200,
+      fixture: "fixtureData.json"})
+      .visit("http://localhost:3000/")
   })
+
+  it('should go to a base url', () => {
+    cy.url().should('include', '/')
+  })
+
+  it('should render a heading', () => {
+    cy.contains("h1", "URL Shortener")
+  })
+
 })
